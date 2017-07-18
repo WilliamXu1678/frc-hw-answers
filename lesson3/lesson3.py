d3eb4a3437eeddcc9e4dd8b25264ca31d2c388d0
jishnusen <jishnu1@gmail.com>
2017-07-18 16:57:49 -0700

    Add answers to lesson 3

---
+++ lesson3/lesson3.py
@@ -0,0 +1,4 @@
+print "1 * 2.5 = ", float(1 * 2.5)
+print "If I try to convert 4.6 to an int it always rounds down to 4: ", int(4.6)
+print "Converting a number to a string always works: ", str(4.6)
+print "But not the other way around if the string isn't a number!", float(str(4.6))
