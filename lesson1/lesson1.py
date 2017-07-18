2d876e65654f3f5c61063ee1a948baf41ad576fc
jishnusen <jishnu1@gmail.com>
2017-07-18 16:10:35 -0700

    Add answers for lesson 1

---
+++ lesson1/lesson1.py
@@ -0,0 +1,3 @@
+print "Jishnu Sen" # My full name will be printed on a line
+print 15 # My age will be printed on a line
+print 15 * 12 # My age times 12 will be printed, giving how many months old I am!
