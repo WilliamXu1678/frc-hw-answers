5072310cadcd8243b976f0912bbd7613c85de3f1
jishnusen <jishnu1@gmail.com>
2017-07-18 16:45:22 -0700

    Add answers for lesson 2

---
+++ lesson2/lesson2.py
@@ -0,0 +1,5 @@
+pizza = int(raw_input("How many pizzas would you like? ")) #This line queries the user how many pizzas they would like, and saves it to the `pizza` variable
+toppings = int(raw_input("How many toppings would you like? ") #This line does the same as above, but stores the given vaule in the toppings var
+
+print "You asked for " + pizza " pizzas and " + toppings + " toppings!"
+#For example, this could print "You asked for for 4 pizzas and 8 toppings!"
