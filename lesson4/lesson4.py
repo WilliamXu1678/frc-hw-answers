f80316f62d246ba440a139f5441bb6504804ce12
jishnusen <jishnu1@gmail.com>
2017-07-18 17:10:55 -0700

    Add answers to lesson 4

---
+++ lesson4/lesson4.py
@@ -0,0 +1,10 @@
+age = int(raw_input("How old are you? "))
+
+if age < 13:
+    print "You're young!"
+elif age < 16:
+    print "You're not too young, but you can't drive :(. BUT you can join the robotics team and drive our robot!!!"
+elif age < 18:
+    print "Congrats, you can drive but you're still a minor!"
+elif age > 21:
+    print "Adult in the house!!!"
