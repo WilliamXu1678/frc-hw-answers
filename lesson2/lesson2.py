25dcc8853dbab7988c268fffd3ba143ef0749145
jishnusen <jishnu1@gmail.com>
2017-07-18 17:25:16 -0700

    Fix Lesson 2 solution

--- lesson2/lesson2.py
+++ lesson2/lesson2.py
@@ -1,5 +1,6 @@
+name = str(raw_input("What's your name? ")) #Asks for the user's name and stores it in the name var
 pizza = int(raw_input("How many pizzas would you like? ")) #This line queries the user how many pizzas they would like, and saves it to the `pizza` variable
-toppings = int(raw_input("How many toppings would you like? ") #This line does the same as above, but stores the given vaule in the toppings var
-
-print "You asked for " + pizza " pizzas and " + toppings + " toppings!"
+pepperoni = int(raw_input("How much pepperoni would you like? ")) #This line does the same as above, but stores the given vaule in the toppings var
+olives = int(raw_input("How many olives would you like? ")) #Same as above, but for olives
+print name, " ordered ", pizza, " pizzas with ", pepperoni, "pepperoni and ", olives, " olives!"
 #For example, this could print "You asked for for 4 pizzas and 8 toppings!"
