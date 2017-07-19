3d7b6ab4285d5cb2c1f620bbecba536f4eeaa467
Livy <livytaylor47@gmail.com>
2017-07-19 16:46:17 -0700

    Remove swp, add space to input

--- lesson6/lesson6problem1.py
+++ lesson6/lesson6problem1.py
@@ -2,7 +2,7 @@ input_array = []
 user_input = ""
 
 while user_input != "stop":
-    user_input = raw_input("Give me a word:")
+    user_input = raw_input("Give me a word: ")
     if user_input != "stop":
        input_array.append(user_input)
 
